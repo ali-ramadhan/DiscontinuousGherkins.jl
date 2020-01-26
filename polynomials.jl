8e815dbdd8986ca51b0f56c0ce6bd0456b2c2530
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-25 22:50:21 -0500

    Even sweeter implementation of the Jacobi polynomials

--- polynomials.jl
+++ polynomials.jl
@@ -9,19 +9,12 @@ b(α, β, n) = -(α^2 - β^2) / (2n+α+β) / (2n+α+β+2)
 
 " Evaluates the Jacobi polynomial P_n^(α,β) at the point x. "
 function jacobi(x, α, β, n::Int)
-    P₀ᵅᵝ = √(2.0^-(α+β+1) * Γ(α+β+2) / Γ(α+1) / Γ(β+1))
-    n == 0 && return P₀ᵅᵝ
-
-    P₁ᵅᵝ = P₀ᵅᵝ/2 * √((α+β+3) / (α+1) / (β+1)) * ((α+β+2)*x + α - β)
-    n == 1 && return P₁ᵅᵝ
-
-    Pᵅᵝ = OffsetArray(zeros(n+1), 0:n)
-    Pᵅᵝ[0] = P₀ᵅᵝ
-    Pᵅᵝ[1] = P₁ᵅᵝ
+    Pᵅᵝ = OffsetArray(zeros(max(2, n+1)), 0:n)
+    Pᵅᵝ[0] = √(2.0^-(α+β+1) * Γ(α+β+2) / Γ(α+1) / Γ(β+1))
+    Pᵅᵝ[1] = P₀ᵅᵝ/2 * √((α+β+3) / (α+1) / (β+1)) * ((α+β+2)*x + α - β)
     for n′ in 1:n-1
         Pᵅᵝ[n′+1] = ((x - b(α,β,n′)) * Pᵅᵝ[n′] - a(α,β,n′) * Pᵅᵝ[n′-1]) / a(α, β, n′+1)
     end
-    
     return Pᵅᵝ[n]
 end
 
