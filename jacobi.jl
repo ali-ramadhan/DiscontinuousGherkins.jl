5cb8cb2fe368f5594fb229dd2c250fc24e838a16
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-26 10:31:59 -0500

    Fix Jacobi polynomials for n < 2

--- jacobi.jl
+++ jacobi.jl
@@ -10,7 +10,7 @@ b(α, β, n) = -(α^2 - β^2) / (2n+α+β) / (2n+α+β+2)
 
 " Evaluates the Jacobi polynomial Pₙᵅᵝ(x). "
 function jacobi(x, α, β, n::Int)
-    Pᵅᵝ = OffsetArray(zeros(max(2, n+1)), 0:n)
+    Pᵅᵝ = n <= 1 ? OffsetArray(zeros(2), 0:1) : OffsetArray(zeros(n+1), 0:n)
     Pᵅᵝ[0] = √(2.0^-(α+β+1) * Γ(α+β+2) / Γ(α+1) / Γ(β+1))
     Pᵅᵝ[1] = Pᵅᵝ[0]/2 * √((α+β+3) / (α+1) / (β+1)) * ((α+β+2)*x + α - β)
     for n′ in 1:n-1
